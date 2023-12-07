b810b2b58ab1680083684de16137098d764d8169
Pedro Victori <21984609+pedrovictori@users.noreply.github.com>
2023-12-07 17:19:09 +0000

    document

--- R/ggplot.R
+++ R/ggplot.R
@@ -1,3 +1,10 @@
+#' Rotate the x axis labels
+#' Helper function
+#' @param angle
+#' @param hjust
+#'
+#' @return
+#' @export
 rotate_x_axis_labels = function(angle = 45, hjust = 1){
   return(theme(axis.text.x = element_text(angle = angle, hjust = hjust)))
 }
